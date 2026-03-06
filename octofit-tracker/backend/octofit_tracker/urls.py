54b6f45575079f1eee351389572fd7d182c6839a
Nick Bento <nick.bento@logicinfo.com>
2026-03-06 15:34:24 +0000

    Use CODESPACE_NAME for root API redirect URL
    - Build absolute root redirect URL from CODESPACE_NAME in Django urls.py
    
    - Keep localhost fallback when CODESPACE_NAME is unset

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,12 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
 
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    api_root_url = f"https://{codespace_name}-8000.app.github.dev/api/"
+else:
+    api_root_url = "http://localhost:8000/api/"
+
 urlpatterns = [
-    path('', RedirectView.as_view(url='/api/', permanent=False)),
+    path('', RedirectView.as_view(url=api_root_url, permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('octofit_tracker.api_urls')),
 ]
